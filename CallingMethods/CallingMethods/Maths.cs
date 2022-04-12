13dd3d0bae2865c3916c0d593c9e6683677884de
salxvador <sal.salvador@gmail.com>
2022-04-12 16:07:04 -0400

    Added CallingMethods assignment.

---
+++ CallingMethods/CallingMethods/Maths.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Maths
+    { 
+        //define properties of class (one int)
+        public int number { get; set; }
+        //method to add two to the number
+        public int addTwo()
+        {
+            int answer = number + 2;
+            return answer;
+        }
+        //method to multiply number by 2
+        public int multTwo()
+        {
+            int answer = number * 2;
+            return answer;
+        }
+        //method to divide number by two
+        public int divTwo()
+        {
+            int answer = number / 2;
+            return answer;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // gather number input from user
+            Console.WriteLine("Please enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            // use input to create new maths class, and assign it to number
+            Maths numb = new Maths();
+            numb.number = num;
+
+            //Call each method:
+            Console.WriteLine("addTwo method: "+numb.addTwo());
+            Console.WriteLine("multTwo method: " + numb.multTwo());
+            Console.WriteLine("divTwo method: " + numb.divTwo());
+            //keep console open
+            Console.Read();
+        }
+    }
+}
