217f6875f77d25666f53b37228f6068543859d09
salxvador <sal.salvador@gmail.com>
2022-04-12 08:32:08 -0400

    added logical comparison loops

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -29,18 +29,44 @@ class Program
         // counter variable
         int j = 0;
 
+        // Use < operator to determine if loop should continue
         while (j < 10)
         {
             numList.Add(j);
             // must increment j with each pass or loop will go on forever.
             j++;
         }
+        // Mark where this print statement begins in the console
+        Console.WriteLine("Start of While-Loop  Int List:");
 
         // Print each element of the list
         foreach (int num in numList)
         {
             Console.WriteLine(num);
         }
+
+        // Another empty list
+        List<string> strList = new List<string>();
+        int counter = 0;
+        // will use this too keep adding pipes to list entries
+        string pipe = "|";
+
+        // Use <= opertaor to keep adding elements to the list
+        while (counter <= 10)
+        {
+            strList.Add(pipe);
+            pipe += "|"; // adds another | to the sequence
+            counter++;
+        }
+        // Mark where this print statement begins in console
+        Console.WriteLine("Start of while loop <= operator with string list:");
+
+        //Print out the above list
+        foreach (string item in strList)
+        {
+            Console.WriteLine(item);
+        }
+
         // Keep console window open for reading
         Console.Read();
     }
