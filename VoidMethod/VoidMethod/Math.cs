99fd392322bd6a0d2fe9e009143d1b33f6cf4281
salxvador <sal.salvador@gmail.com>
2022-04-13 10:51:59 -0400

    Added VoidMethod Class Method assignment;

---
+++ VoidMethod/VoidMethod/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Math
+    {
+        //Create void method named compute
+        public void compute(int operand, int num2)
+        {
+            //do math on the first argument and print the result; print the second argument.
+            int sum = operand + 12;
+            Console.WriteLine("The sum of the first argument and 12 is: " + sum);
+            Console.WriteLine("The second argument is " + num2);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the Math object
+            Math math = new Math();
+
+            //Call the compute method passing in 2 args
+            math.compute(40, 16);
+
+            //Call the compute method specifying params by name
+            math.compute(operand: 14, num2: 65);
+
+            //Keep console open
+            Console.Read();
+        }
+    }
+}
