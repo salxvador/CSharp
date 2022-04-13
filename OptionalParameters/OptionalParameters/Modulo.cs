e7ce583afa2bb0a734067dff2384a7505fa0acdd
salxvador <sal.salvador@gmail.com>
2022-04-13 10:26:32 -0400

    Added OptionalParameters assignment

---
+++ OptionalParameters/OptionalParameters/Modulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    public class Modulo
+    {
+        //Method takes input and modulates by 2 as default.
+        //optional parameter can replace 2 as modulator
+        public int Modulate(int num1, int num2 = 2)
+        {
+            int ans = num1 % num2;
+            return ans;
+        }
+    }
+}
---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate Modulo class
+            Modulo mod = new Modulo();
+
+            //Collect required input num1
+            Console.WriteLine("Please enter a whole number to be modulated: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Collect optional input as modulator
+            Console.WriteLine("Optional: enter a whole number modulator. 2 will be used by default: ");
+            
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                //Perform modulation with both arguments, print result
+                int answer = mod.Modulate(num1, num2);
+                Console.WriteLine(num1 + " modulus " + num2 + " equals: " + answer);
+            }
+            catch (Exception) // exception is thrown if no value is entered.
+            {
+                //Perform modulation with only 1 argument, print ressult
+                int ans = mod.Modulate(num1);
+                Console.WriteLine(num1 + " modulus 2 equals: " + ans);
+            }
+
+            //Keep Console open
+            Console.Read();
+        }
+    }
+}
