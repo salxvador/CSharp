07932bcb8b1c67e8739a1d6c5cd78d058e3ae7da
salxvador <sal.salvador@gmail.com>
2022-04-13 10:00:44 -0400

    Added Main Method (method overloading) assignment

---
+++ MainMethod/MainMethod/Maths.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Maths
+    {
+        //First Method, add 5 with integer input and output
+        public int doMath(int number)
+        {
+            //This version of doMath adds 5 to an integer
+            int answer = number + 5;
+            return answer;
+        }
+
+        //Second Method, do math with decimal input and integer output
+        public int doMath(decimal number)
+        {
+            //This version of doMath multiplies decimal by 2, and returns int
+            int answer = Convert.ToInt32(number * 2);
+            return answer;
+        }
+
+        //Third Method, do math with string input and return integer
+        public int doMath(string number)
+        {
+            //this version of doMath converts string to int, and divides it by two
+            int answer = Convert.ToInt32(number) / 2;
+            return answer;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the Maths Class
+            Maths num = new Maths();
+
+            //Create integer, decimal, and string variables
+            int integer = 7;
+            decimal dec = 4.62m;
+            string str = "14";
+
+            //print the result of the first method (add 5 using int)
+            Console.WriteLine(num.doMath(integer));
+
+            //print result of the second method (multiply decimal by 2, return int)
+            Console.WriteLine(num.doMath(dec));
+
+            //print result of the third method (divide string number by two, return int)
+            Console.WriteLine(num.doMath(str));
+
+            //Keep Console window open
+            Console.Read();
+        }
+    }
+}
