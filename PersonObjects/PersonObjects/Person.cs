936cf4a9277a3cf67bdef0b2863f538178a4cd6a
salxvador <sal.salvador@gmail.com>
2022-04-13 13:15:35 -0400

    Added Person Object Assignment (pg 292)

---
+++ PersonObjects/PersonObjects/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonObjects
+{
+    public class Person
+    {
+        //add first/last name properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //add sayName method
+        public void SayName()
+        {
+            //Format string to print first and last names
+            Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+        }
+    }
+}
---
+++ PersonObjects/PersonObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate and initialize Employee object
+            Employee emp = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            //Call SayName() method (from super class)
+            emp.SayName();
+
+            //Keep Console Open
+            Console.Read();
+        }
+    }
+}
