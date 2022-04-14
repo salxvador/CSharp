5fc6b95a509ac60895c75bdde709dff5214583b5
salxvador <sal.salvador@gmail.com>
2022-04-14 13:40:59 -0400

    Added Struct assignment (pg 331)

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate number object from struct
+            Number num = new Number() { Amount = 53.62m };
+
+            //print the amount
+            Console.WriteLine(num.Amount);
+            Console.Read();
+        }
+    }
+}
