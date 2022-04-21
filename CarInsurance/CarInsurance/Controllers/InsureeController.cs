4ab6dcfb411f43b3c8488b5bdc0655774ea59bf9
salxvador <sal.salvador@gmail.com>
2022-04-21 11:59:03 -0400

    added Quote Calculation logic and edited view

--- CarInsurance/CarInsurance/Controllers/InsureeController.cs
+++ CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -48,8 +48,43 @@ namespace CarInsurance.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,DateOfBirth,CarYear,CarMake,CarModel,DUI,SpeedingTickets,CoverageType,QUOTE")] Insuree insuree)
         {
+            // declare value for quote since I removed it from view form
+            insuree.QUOTE = 50;
+
             if (ModelState.IsValid)
             {
+                // age calculations. all diffs are in DAYS
+                TimeSpan age = DateTime.Now - insuree.DateOfBirth;
+                // add 50 for being under 25, another 50 for being under 18:
+                if (age.Days < 9131) { insuree.QUOTE += 50; }
+                if (age.Days < 6574) { insuree.QUOTE += 50; }
+                //add 25 for being over 25
+                if (age.Days > 6574) { insuree.QUOTE += 25; }
+
+                // Car age evaluations
+                if (insuree.CarYear < 2000) { insuree.QUOTE += 25; }
+                if (insuree.CarYear > 2015) { insuree.QUOTE += 25; }
+
+                // Evals for porsche and porsche carrera
+                if (insuree.CarMake.ToLower() == "porsche") { insuree.QUOTE += 25; }
+                if (insuree.CarMake.ToLower() == "porsche" && insuree.CarModel.ToLower() == "carrera")
+                {
+                    insuree.QUOTE += 25;
+                }
+
+                // Add 10 for each speeding ticket
+                for (int i=0; i<insuree.SpeedingTickets; i++)
+                {
+                    insuree.QUOTE += 10;
+                }
+
+                // add 25% for DUI
+                if (insuree.DUI) { insuree.QUOTE = insuree.QUOTE * 1.25m; }
+
+                // add 50% for full coverage (CoverageType)
+                if (insuree.CoverageType) { insuree.QUOTE = insuree.QUOTE * 1.5m; }
+
+                // add object after quote has been calculated, save changes.
                 db.Insurees.Add(insuree);
                 db.SaveChanges();
                 return RedirectToAction("Index");
